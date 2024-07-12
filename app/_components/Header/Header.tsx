1c1eaff1f06157fa0ecdca08110d5f28a12b83f8
jiri.grussmann <jiri.grussmann@3pillarglobal.com>
2024-07-12 15:24:51 +0200

    Init commit multitenant app

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -1,18 +1,20 @@
 import { type JSX } from 'react'
-import MinimalisticHeader from "@/app/_components/Header/minimalistic/Header";
-import ModernHeader from "@/app/_components/Header/modern/Header";
 
 export type HeaderProps = {
   title: string
 }
 const type: string = 'modern'
 
-export default function Header(props: HeaderProps): JSX.Element | null {
+export default async function Header(props: HeaderProps): Promise<JSX.Element | null> {
   switch (type) {
     case 'minimalistic':
-      return <MinimalisticHeader {...props} />
+      return await import(
+        '@/app/_components/Header/minimalistic/Header'
+      ).then((module) => module.default(props))
     case 'modern':
-      return <ModernHeader {...props} />
+      return await import(
+        '@/app/_components/Header/modern/Header'
+      ).then((module) => module.default(props))
     default:
       return null
   }
